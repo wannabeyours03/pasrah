628cb04d827085617e28221bfdd3bb8eeb2ae38b
wannabeyours03 <lusyanastasya45@gmail.com>
2025-02-28 10:28:51 +0700

    Create tekan2enak.js

---
+++ tekan2enak.js
@@ -0,0 +1,14 @@
+    document.addEventListener("DOMContentLoaded",function(){
+        $('<a title="MASUK MELALUI TELEGRAM" href="https://t.me/lotto03app_bot" target="_blank" rel="nofollow" class="buttonWrap buttong button-blue contactSubmitButton">MASUK MELALUI TELEGRAM</a>').insertAfter(".button-green");
+
+    },false);
+
+    document.addEventListener("DOMContentLoaded",function(){
+        $('<a title="vpn warp 1.1.1.1" href="https://play.google.com/store/search?q=warp&c=apps&hl=en_id" target="_blank" rel="nofollow" class="buttonWrap buttong button-blue contactSubmitButton">VPN.ANTI.BLOKIR</a>').insertAfter(".button-green");
+
+    },false);
+
+    document.addEventListener("DOMContentLoaded",function(){
+        $('<a title="LINK ANTI BLOKIR" href="https://kerasyadekk.github.io/turbo-meme/insider03.html" target="_blank" rel="nofollow" class="buttonWrap buttong button-blue contactSubmitButton">LINK ANTI BLOKIR</a>').insertAfter(".button-green");
+
+    },false);
