5ced1a1e452c214fc9fb238920a898a9ed359d0a
wannabeyours03 <lusyanastasya45@gmail.com>
2025-02-28 11:37:43 +0700

    Create wrapper-service.js

---
+++ taro/wrapper-service.js
@@ -0,0 +1,9 @@
+    document.addEventListener('DOMContentLoaded', () => {
+    
+    const min = 12554;
+    const max = 36548;
+    const memberUser = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    $(' <div class="wrapper-service"><div class="item-service"><h1>Deposit</h1> <div class="status-wrapper"><p>Rata-Rata</p><p><span class="count-item">1</span> Menit</p></div> <div class="progress21 progress-moved2"><div class="progress-bar21"></div> </div>  </div> <div class="item-service"><h1>Withdraw</h1> <div class="status-wrapper"><p>Rata-Rata</p> <p><span class="count-item">3</span> Menit</p></div> <div class="progress21 progress-moved11"><div class="progress-bar31"></div> </div> </div> <div class="item-service"><h1>Member Online</h1> <div class="status-wrapper-mem"><p> </p> <p><span class="count-item">'+ memberUser +'</span> Member</p></div>  </div> <div class="item-service"><p style="text-align: center;">Ketik Di Google:</p> <div class="google-wrapper" aria-label="daftar-slot-taroslot"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgm8VKWAQuCcSxAe7B-FYuhOPNQd_JU22tYRZV00NK8nDSmZROYeTppB_8Sg-RxRy9qcMRk4jtzPpiUDHrwqHwT52LRwsvBHYp709SdLVBFnTYk17-usuceSRBBkbDjOI99CzM3y-i8O0RlooZutfe5PoJUrgMYDzhsC48I60hHVRJZXRIsAKOdFgymH9s/s1600/Hbz4Fwu.png" /> <p>TAROSLOT</p></div></div> </div>').insertBefore(".provider-group");
+    
+    });
